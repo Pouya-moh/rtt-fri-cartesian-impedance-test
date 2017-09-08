9daa5e7576c43faa7a3fab8901e8c289fdb9f391
pouya-moh <other.pouya@gmail.com>
2017-09-08 11:05:24 +0200

    add ports

--- include/CartesianImpedance.hpp
+++ include/CartesianImpedance.hpp
@@ -2,14 +2,33 @@
 #define CARTESIANIMPEDANCE_HPP
 
 #include <rtt/RTT.hpp>
+#include <rst-rt/kinematics/JointAngles.hpp>
+#include <rst-rt/dynamics/JointTorques.hpp>
+#include <kdl/frames.hpp>
 
 class CartesianImpedance : public RTT::TaskContext{
-  public:
+public:
     CartesianImpedance(std::string const& name);
     bool configureHook();
     bool startHook();
     void updateHook();
     void stopHook();
     void cleanupHook();
+
+private:
+    RTT::OutputPort<rstrt::kinematics::JointAngles> red_res_out_port;
+    rstrt::kinematics::JointAngles                  red_res_out_data;
+
+    RTT::OutputPort<rstrt::dynamics::JointTorques> ext_trq_out_port;
+    rstrt::dynamics::JointTorques                  ext_trq_out_data;
+
+    RTT::OutputPort<KDL::Frame> cart_pose_out_port, cart_pose_loop_out_port;
+    KDL::Frame                  cart_pose_out_data, cart_pose_loop_out_data;
+
+    RTT::InputPort<KDL::Frame> cur_cart_pose_in_port;
+    RTT::FlowStatus            cur_cart_pose_in_flow;
+    KDL::Frame                 cur_cart_pose_in_data;
+
+    void initializePorts();
 };
 #endif
--- src/CartesianImpedance.cpp
+++ src/CartesianImpedance.cpp
@@ -22,6 +22,33 @@ void CartesianImpedance::stopHook() {
 void CartesianImpedance::cleanupHook() {
 }
 
+void CartesianImpedance::initializePorts(){
+	cur_cart_pose_in_flow = RTT::NoData;
+    cur_cart_pose_in_port.setName("cur_cart_pose_in_port");
+    cur_cart_pose_in_data = KDL::Frame();
+    ports()->addPort(cur_cart_pose_in_port);
+
+    cart_pose_out_data = KDL::Frame();
+    cart_pose_out_port.setName("cart_pose_out_port");
+    cart_pose_out_port.setDataSample(cart_pose_out_data);
+    ports()->addPort(cart_pose_out_port);
+
+    cart_pose_loop_out_data = KDL::Frame();
+    cart_pose_loop_out_port.setName("cart_pose_loop_out_port");
+    cart_pose_loop_out_port.setDataSample(cart_pose_loop_out_data);
+    ports()->addPort(cart_pose_loop_out_port);
+
+    red_res_out_data = rstrt::kinematics::JointAngles(DOF_SIZE);
+    red_res_out_port.setName("red_res_out_port");
+    red_res_out_port.setDataSample(red_res_out_data);
+    ports()->addPort(red_res_out_port);
+
+    ext_trq_out_data = rstrt::dynamics::JointTorques(DOF_SIZE);
+    ext_trq_out_port.setName("ext_trq_out_port");
+    ext_trq_out_port.setDataSample(ext_trq_out_data);
+    ports()->addPort(ext_trq_out_port);
+}
+
 /*
  * Using this macro, only one component may live
  * in one library *and* you may *not* link this library
